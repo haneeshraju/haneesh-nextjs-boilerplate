96518df6345947b10787d26da5b961521be0118d
haneeshraju <112978983+haneeshraju@users.noreply.github.com>
2022-11-08 19:45:34 -0500

    updated username.

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Page owned by satmaku
+          Page owned by vdandu
         </p>
 
         <div className={styles.grid}>
